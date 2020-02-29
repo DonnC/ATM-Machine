a5774dfa33b338672fca6f0c0b7aeced50962fef
Donald Chinhuru <donychinhuru@gmail.com>
2020-02-29 08:02:56 +0200

    Update WelcomeScreen.java

--- src/WelcomeScreen.java
+++ src/WelcomeScreen.java
@@ -1,11 +1,11 @@
 
 public class WelcomeScreen {
-	private String student_number = "N0173320W";
-	private String student_name   = "DONALD CHINHURU";
+	private String student_number = "xxxxxxx"
+	private String student_name   = "DONALD C";
 	private String course_code    = "TEE 3133";
 	private String course_name    = "SOFTWARE ENGINEERING APPLICATIONS";
 	private String ass_name       = "ATM MACHINE WITH CUSTOM EXCEPTIONS";
-	private int    year			  = 2019;
+	private int    year	      = 2019;
 	private String semester       = "THIRD YEAR. PART 3.1";
 	
 	WelcomeScreen()
